e476549bd40f2e106257a2aac7913917986f9276
EugeneMorash <85665094+EugeneMorash@users.noreply.github.com>
2022-05-04 12:08:50 +0300

    mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import {TodoList} from "./Todolist";
 
+export type TasksArr = Array<LangType>;
+export type LangType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+const tasks: TasksArr = [
+    {id: 100, title: "HTML", isDone: true},
+    {id: 101, title: "CSS", isDone: true},
+    {id: 102, title: "JS", isDone: true},
+    {id: 103, title: "TS", isDone: false},
+    {id: 104, title: "React", isDone: false},
+    {id: 105, title: "Redux", isDone: false},
+]
+
+
 
 
 
 function App() {
+
+
     return (
         <div>
-            <AppTitle subheading={'Many TODOLIST!!'}/>
-            <TodoList title={"Junior"} />
-            <TodoList title={"Cook"}/>
+            <TodoList title={"Junior"} taskList={tasks}/>
+            {/*<TodoList title={"Cook"}/>*/}
         </div>
     );
 }
 
 
-type AppTitlePropsType = {
-    subheading: string
-}
 
-
-function AppTitle(props: AppTitlePropsType) {
-    return (
-        <h2>{props.subheading}</h2>
-    )
-}
 
 export default App;
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {TasksArr} from "./App";
 
 type TodoListPropsType = {
     title: string;
+    taskList: TasksArr;
 }
 
 export function TodoList(props: TodoListPropsType) {
+    const taskArr = props.taskList.map(task => {
+        return (
+            <li key={task.id}>
+                <input type="checkbox" checked={task.isDone}/>
+                <span>{task.title}</span>
+            </li>
+        )
+    })
+
     return (
         <div className="todoList">
             <h3>{props.title}</h3>
@@ -13,22 +24,24 @@ export function TodoList(props: TodoListPropsType) {
                 <button>Add</button>
             </div>
             <ul>
-                <li>
-                    <input type="checkbox" checked={true}/>
-                    <span>HTML</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={true}/>
-                    <span>CSS</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={true}/>
-                    <span>JS</span>
-                </li>
-                <li>
-                    <input type="checkbox"/>
-                    <span>TS</span>
-                </li>
+                {taskArr}
+
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={true}/>*/}
+                {/*    <span>HTML</span>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={true}/>*/}
+                {/*    <span>CSS</span>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={true}/>*/}
+                {/*    <span>JS</span>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox"/>*/}
+                {/*    <span>TS</span>*/}
+                {/*</li>*/}
             </ul>
             <div>
                 <button>All</button>
